6a8389111aadaa2f56c9d560aa3c6828343e5530
PavelVlasenko <vlasenko-pav@yandex.ru>
2015-06-24 18:11:27 +0700

    Dag generator

--- src/DagGenerator.java
+++ src/DagGenerator.java
@@ -16,10 +16,12 @@ public class DagGenerator
         //create triangle matrix
         int [][] matrix = new int[vertex][vertex];
         for(int i = 0; i < vertex; i++) {
+            boolean edgeExist = false;
             for (int j = i+1; j < vertex; j++) {
                 //maybePutAnEdgeBetween(i, j);
                 Random random = new Random();
-                matrix[i][j] = random.nextInt(1);
+                int value = random.nextInt(2);
+                matrix[i][j] = random.nextInt(2);
             }
         }
 
